95859805dabeee560ea39a71a7e6925d9a7c7eac
Evan Zona <evanbzona@gmail.com>
2022-02-28 10:29:15 -0600

    Initial Commit

---
+++ 'Za hunter/_Za_hunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_hunterApp.swift
+//  'Za hunter
+//
+//  Created by Student on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct _Za_hunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
